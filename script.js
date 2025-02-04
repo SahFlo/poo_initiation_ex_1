42b003fe65b33f8b218a43bd211d7d65cba689b2
SahFlo <sahal.florence@gmail.com>
2025-02-04 15:28:51 +0100

    un peu d'aide de l'IA mais j'ai compris la mécanique

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-//constructeur de classe
+//Etape 1 - constructeurs de classe
+//convention : les noms de classe commencent par une majuscule
 class Film {
     constructor(titre, dateSortie, realisatrice) {
         this.titre = titre;
@@ -8,3 +9,28 @@ class Film {
     }
 }
 
+class Realisatrice {
+    constructor(nom, prenom, anneeNaissance) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.anneeNaissance = anneeNaissance;
+    }
+}
+
+function aboutFilm(film) {
+    console.log(`le film ${film.titre} est sorti en ${film.dateSortie} et est réalisé par ${film.realisatrice.prenom} ${film.realisatrice.nom}`)
+}
+
+// Création des instances
+const real1 = new Realisatrice("Triet", "Justine", 1978);
+const real2 = new Realisatrice("Campion", "Jane", 1954);
+const real3 = new Realisatrice("Bigelow", "Kathryn", 1951);
+
+const film1 = new Film("Anatomie d’une chute", 2023, real1);
+const film2 = new Film("The Power of the Dog", 2021, real2);
+const film3 = new Film("Démineurs", 2008, real3);
+
+// Test de la fonction
+aboutFilm(film1);
+aboutFilm(film2);
+aboutFilm(film3);
